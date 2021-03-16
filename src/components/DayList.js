2a088ff5755291015234c8142173fbe39e0d19b6
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-03-15 23:39:16 -0600

    Update app to run in browser

---
+++ src/components/DayList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import DayListItem from './DayListItem';
+
+export default function DayList(props) {
+	const days = props.days.map(day => {
+		return (
+			<DayListItem
+				id={day.id}
+				name={day.name}
+				spots={day.spots}
+				selected={day.name === props.day}
+				setDay={props.setDay}
+			/>
+		);
+	});
+
+	return days;
+}
