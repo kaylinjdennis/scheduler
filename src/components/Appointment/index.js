beed6f6b8ffa3bc0dd02222421940e19047342eb
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-03-16 23:44:03 -0600

    Add to appointment function to display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
+
 import 'components/Appointment/styles.scss';
 
 export default function Appointment(props) {
 
-	return <article className="appointment"></article>;
+	return (
+		<article className="appointment">
+			<Header time={props.time} />
+			{props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+		</article>);
 }
