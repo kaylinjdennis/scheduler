f8d2dab98cd342ef531c3629ffcb0ba94ae03043
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-03-16 23:30:29 -0600

    Add state to the form

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,8 @@ import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
 import Status from 'components/Appointment/Status';
 import Error from 'components/Appointment/Error';
+import Form from 'components/Appointment/Form';
+
 
 
 storiesOf("Button", module)
@@ -160,3 +162,19 @@ storiesOf('Appointment', module)
 	))
 	.add('Status', () => <Status message='Deleting' />)
 	.add('Error', () => <Error message='Could not delete appointment.' onClose={action('onClose')} />)
+	.add('Edit', () => (
+		<Form
+			name='Rachel Green'
+			interviewers={interviewers}
+			interviewer={1}
+			onSave={action('onSave')}
+			onCancel={action('onCancel')}
+		/>
+	))
+	.add('Create', () => (
+		<Form
+			interviewers={interviewers}
+			onSave={action('onSave')}
+			onCancel={action('onCancel')}
+		/>
+	))
