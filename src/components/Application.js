e4046e2bd6b463740313af2d513b0bfbfde533cf
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-03-17 00:10:23 -0600

    Add the Appointments to the actual application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,40 +1,107 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import "components/Application.scss";
-import DayListItem from './DayListItem';
+import 'components/Application.scss';
 import DayList from './DayList';
+import Appointment from 'components/Appointment/index';
 
 const days = [
 	{
 		id: 1,
-		name: "Monday",
+		name: 'Monday',
 		spots: 2,
 	},
 	{
 		id: 2,
-		name: "Tuesday",
+		name: 'Tuesday',
 		spots: 5,
 	},
 	{
 		id: 3,
-		name: "Wednesday",
+		name: 'Wednesday',
 		spots: 0,
 	},
 ];
 
+const appointments = [
+	{
+		id: 1,
+		time: '12pm',
+	},
+	{
+		id: 2,
+		time: '1pm',
+		interview: {
+			student: 'Lydia Miller-Jones',
+			interviewer: {
+				id: 1,
+				name: 'Sylvia Palmer',
+				avatar: 'https://i.imgur.com/LpaY82x.png',
+			}
+		}
+	},
+	{
+		id: 3,
+		time: '2pm',
+		interview: {
+			student: 'Jim Halpert',
+			interviewer: {
+				id: 2,
+				name: 'Tori Malcolm',
+				avatar: 'https://i.imgur.com/Nmx0Qxo.png',
+			}
+		}
+	},
+	{
+		id: 4,
+		time: '3pm',
+		interview: {
+			student: 'Micheal Scott',
+			interviewer: {
+				id: 3,
+				name: 'Mildred Nazir',
+				avatar: 'https://i.imgur.com/T2WwVfS.png',
+			}
+		}
+	},
+	{
+		id: 5,
+		time: '4pm'
+	},
+	{
+		id: 6,
+		time: '5pm',
+		interview: {
+			student: 'Dwight Schrute',
+			interviewer: {
+				id: 4,
+				name: 'Cohana Roy',
+				avatar: 'https://i.imgur.com/FK8V841.jpg',
+			}
+		}
+	}
+];
+
 export default function Application(props) {
 	const [day, setDay] = useState('Monday');
+	const appointmentComponents = appointments.map((appointment) => {
+		return (
+			<Appointment
+				key={appointment.id}
+				{...appointment}
+			/>
+		)
+	});
 
 	return (
-		<main className="layout">
-			<section className="sidebar">
+		<main className='layout'>
+			<section className='sidebar'>
 				<img
-					className="sidebar--centered"
-					src="images/logo.png"
-					alt="Interview Scheduler"
+					className='sidebar--centered'
+					src='images/logo.png'
+					alt='Interview Scheduler'
 				/>
-				<hr className="sidebar__separator sidebar--centered" />
-				<nav className="sidebar__menu">
+				<hr className='sidebar__separator sidebar--centered' />
+				<nav className='sidebar__menu'>
 					<DayList
 						days={days}
 						day={day}
@@ -42,13 +109,14 @@ export default function Application(props) {
 					/>
 				</nav>
 				<img
-					className="sidebar__lhl sidebar--centered"
-					src="images/lhl.png"
-					alt="Lighthouse Labs"
+					className='sidebar__lhl sidebar--centered'
+					src='images/lhl.png'
+					alt='Lighthouse Labs'
 				/>
 			</section>
-			<section className="schedule">
-				{/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+			<section className='schedule'>
+				{appointmentComponents}
+				<Appointment key="last" time="6pm" />
 			</section>
 		</main>
 	);
