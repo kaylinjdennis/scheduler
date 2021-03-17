821bf46e510ec96f97d6ab410107535ad4086ba8
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-03-16 22:03:01 -0600

    Create more components for the appointments

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,11 @@ import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment/index';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
+
 
 storiesOf("Button", module)
 	.addParameters({
@@ -138,3 +143,20 @@ storiesOf('Appointment', module)
 	.add('Appointment with Time', () => <Appointment time='12pm' />)
 	.add('Header', () => <Header time='11pm' />)
 	.add('Empty', () => <Empty onAdd={action('onAdd')} />)
+	.add('Show', () => (
+		<Show
+			student='Lydia Miller-Jones'
+			interviewer={interviewers[0]}
+			onEdit={action('onEdit')}
+			onDelete={action('onDelete')}
+		/>
+	))
+	.add('Confirm', () => (
+		<Confirm
+			message='Delete the Appointment?'
+			onConfirm={action('onConfirm')}
+			onCancel={action('onCancel')}
+		/>
+	))
+	.add('Status', () => <Status message='Deleting' />)
+	.add('Error', () => <Error message='Could not delete appointment.' onClose={action('onClose')} />)
