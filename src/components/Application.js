dd72afd96d3c6aa37b17fa279f57d994d4c10ef2
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-03-18 02:52:33 -0600

    Retrieve appointments from api and add them into the app

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,27 +4,29 @@ import axios from 'axios';
 import 'components/Application.scss';
 import DayList from './DayList';
 import Appointment from 'components/Appointment/index';
+import { getAppointmentsForDay } from 'helpers/selectors';
 
 export default function Application(props) {
 	const [state, setState] = useState({
 		day: 'Monday',
 		days: [],
-		appointments: []
+		appointments: {}
 	});
 
 	const setDay = day => setState({ ...state, day });
-	const setDays = days => setState(prev => ({ ...prev, days }));
+
+	const dailyAppointments = getAppointmentsForDay(state, state.day);
 
 	useEffect(() => {
-		axios.get('/api/days')
-			.then(response => {
-				setDays([...response.data]);
-				console.log(response.data);
-			})
-	}, []);
+		Promise.all([
+			axios.get('/api/days'),
+			axios.get('/api/appointments')
+		]).then((all) => {
+			setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+		});
+	}, [state.days]);
 
-	console.log('state.appointments', state.appointments)
-	const appointmentComponents = state.appointments.map((appointment) => {
+	const appointmentComponents = dailyAppointments.map((appointment) => {
 		return (
 			<Appointment
 				key={appointment.id}
