d0741100d0cc86008fb9a254bd89bc9c32c1da90
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-03-18 23:47:21 -0600

    Use custom hook in appointment component to change the mode of the appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,42 @@ import React from 'react';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
+
+import useVisualMode from 'hooks/useVisualMode';
 
 import 'components/Appointment/styles.scss';
 
-export default function Appointment(props) {
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE';
 
+export default function Appointment(props) {
+	const { mode, transition, back } = useVisualMode(
+		props.interview ? SHOW : EMPTY
+	);
 	return (
 		<article className="appointment">
 			<Header time={props.time} />
-			{props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+			{mode === EMPTY &&
+				<Empty
+					onAdd={() => {
+						console.log("Clicked onAdd");
+						transition(CREATE);
+					}}
+				/>
+			}
+			{mode === SHOW && (
+				<Show
+					student={props.interview.student}
+					interviewer={props.interview.interviewer}
+				/>
+			)}
+			{mode === CREATE && (
+				<Form
+					interviewers={[]}
+					onCancel={() => { back() }}
+				/>
+			)}
 		</article>);
 }
