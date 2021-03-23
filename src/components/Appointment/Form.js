ccfc2f1652eb67eecda367c6a0645c48938f947f
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-03-23 01:46:21 -0600

    Use tdd to add message rather than saving if no name given when creating new appointment

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ import Button from 'components/Button';
 export default function Form(props) {
 	const [name, setName] = useState(props.name || '');
 	const [interviewer, setInterviewer] = useState(props.interviewer || null);
+	const [error, setError] = useState('');
 
 	const reset = () => {
 		setName('');
@@ -15,6 +16,14 @@ export default function Form(props) {
 	const cancel = () => {
 		reset();
 		props.onCancel();
+	};
+
+	const validate = () => {
+		if (name === '') {
+			setError('student name cannot be blank');
+			return;
+		}
+		props.onSave(name, interviewer);
 	};
 
 	return (
@@ -28,14 +37,16 @@ export default function Form(props) {
 						placeholder="Enter Student Name"
 						value={name}
 						onChange={(event) => setName(event.target.value)}
+						data-testid='student-name-input'
 					/>
+					<section className='appointment__validation'>{error}</section>
 				</form>
 				<InterviewerList interviewers={props.interviewers} interviewer={interviewer} setInterviewer={setInterviewer} onClick={props.setInterviewer} onSubmit={event => event.preventDefault()} />
 			</section>
 			<section className="appointment__card-right">
 				<section className="appointment__actions">
 					<Button danger onClick={cancel}>Cancel</Button>
-					<Button confirm onClick={() => { props.onSave(name, interviewer) }}>Save</Button>
+					<Button confirm onClick={() => { validate() }}>Save</Button>
 				</section>
 			</section>
 		</main>
