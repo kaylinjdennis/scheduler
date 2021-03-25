b4207958eece6c615437cee0d09ccb5d5a1ec7ac
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-03-25 05:32:02 -0600

    Fix no key error

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,6 +5,7 @@ export default function DayList(props) {
 	const days = props.days.map(day => {
 		return (
 			<DayListItem
+				key={day.id}
 				id={day.id}
 				name={day.name}
 				spots={day.spots}
